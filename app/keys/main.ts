e0bdaefe0a99440994df4d1ba3b4693618709dc9
Nikita Krikun <ndkrikun@gmail.com>
2018-01-28 02:01:53 +0300

    Fixed time interval

--- app/keys/main.ts
+++ app/keys/main.ts
@@ -13,10 +13,10 @@ export const ACCESS_TOKEN: string = 'asdfa3ofaasodfai32iasdfasf';
  */
 type CandlesPeriod = 'M1' | 'M3' | 'M5' | 'M15' | 'M30' | 'H1' | 'H4' | 'D1' | 'D7' | '1M'
 
-const MINUTES_INTERVAL = 3;
+const MINUTES_INTERVAL = 1;
 
 export const MS_INTERVAL: number = 1000 * 60 * MINUTES_INTERVAL;
 
-export const CANDLES_QUANTITY: number = (24 * 60) / MINUTES_INTERVAL; // Max 1000
+export const CANDLES_QUANTITY: number = (12 * 60) / MINUTES_INTERVAL; // For 12 hrs
 
 export const CANDLES_PERIOD: CandlesPeriod = `M${MINUTES_INTERVAL}` as CandlesPeriod;
